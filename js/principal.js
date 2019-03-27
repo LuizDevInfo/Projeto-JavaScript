fb2c76b0177218e2d6ae747541d69b09800acb62
Luiz Gustavo <luizgustavofachinetto@hotmail.com>
2019-03-27 18:19:15 -0300

    AulaScriptVs2

--- js/principal.js
+++ js/principal.js
@@ -5,35 +5,39 @@ titulo.textContent = "Aparecida Nutricionista"
 var pacientes = document.querySelectorAll(".paciente");
 
 for(var i = 0; i < pacientes.length; i++){
-
+    console.log(pacientes);
     var paciente = pacientes[i];
 
-var tdpeso = paciente.querySelector(".info-peso");
-var peso = tdpeso.textContent;
+    var tdpeso = paciente.querySelector(".info-peso");
+    var peso = tdpeso.textContent;
 
-var tdaltura = paciente.querySelector(".info-altura");
-var altura = tdaltura.textContent;
+    var tdaltura = paciente.querySelector(".info-altura");
+    var altura = tdaltura.textContent;
 
-var tdimc = paciente.querySelector(".info-imc");
+    var tdImc = paciente.querySelector(".info-imc");
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+    var console = console
+    if(peso <= 0 || peso >= 500){
+        console.log("Peso invalido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso invalido!";
+        paciente.classList.add("paciente-invalido");
+    }
 
-if(peso < 0 || peso > 500){
-    console.log("Peso invalido");
-    pesoEhValido = false;
-}
+    if(altura <= 0 || altura >= 3.00){
+        console.log("Altura Invalida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura invalido!";
+        paciente.classList.add("paciente-invalido");
+    }
 
-if(altura < 0 || altura > 3.00){
-    console.log("Altura Invalida");
-    alturaEhValida = false;
-}
-
-if(alturaEhValida && pesoEhValido){
-    var imc = peso / (altura * altura);
-   tdimc.textContent = imc;
-}
-    
+    if(alturaEhValida && pesoEhValido){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+            
 }
 
 
